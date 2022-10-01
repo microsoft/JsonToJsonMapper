322e503251de49b57adfab428a8c3e3bdc2ebbe0
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-10-01 11:10:22 +0100

    - skeleton UI

---
+++ src/JsonToJsonMapper.ScriptTester.Core/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace JsonToJsonMapper.ScriptTester.Core.Pages;
+
+public sealed partial class Index
+{
+  private string _code { get; set; }
+  private string _assemblies { get; set; }
+  private string _namespaces { get; set; }
+  private string _input { get; set; }
+  private string _output { get; set; }
+  private bool _canRun => !string.IsNullOrWhiteSpace(_code) && !string.IsNullOrWhiteSpace(_input);
+
+  private Task OnRun()
+  {
+    _output = $"Run:  {_code}{Environment.NewLine}+{Environment.NewLine}{_input}";
+    return Task.CompletedTask;
+  }
+}
