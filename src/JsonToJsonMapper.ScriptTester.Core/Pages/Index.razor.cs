5dd25a7e397261a21d4763a94126746de1318493
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-10-01 14:20:41 +0100

    - integrate Monaco code editor for better UI - always enable Run button -- Monaco returns text async -- Blazor does not support async validation etc

--- src/JsonToJsonMapper.ScriptTester.Core/Pages/Index.razor.cs
+++ src/JsonToJsonMapper.ScriptTester.Core/Pages/Index.razor.cs
@@ -1,27 +1,41 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
-using Microsoft.CodeAnalysis.Scripting;
+﻿namespace JsonToJsonMapper.ScriptTester.Core.Pages;
 
-namespace JsonToJsonMapper.ScriptTester.Core.Pages;
+using BlazorMonaco;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 
 public sealed partial class Index
 {
-  private string _code { get; set; }
+  private MonacoEditor _code { get; set; }
   private string _assemblies { get; set; }
   private string _namespaces { get; set; }
   private string _input { get; set; }
   private string _output { get; set; }
-  private bool _canRun => !string.IsNullOrWhiteSpace(_code) && !string.IsNullOrWhiteSpace(_input);
+
+  private StandaloneEditorConstructionOptions EditorOptions(MonacoEditor editor)
+  {
+    return new StandaloneEditorConstructionOptions
+    {
+      Language = "csharp",
+      AutomaticLayout = true,
+      RenderIndentGuides = false,
+      RenderFinalNewline = false,
+      ColorDecorators = true,
+      OccurrencesHighlight = true,
+    };
+  }
 
   private async Task OnRun()
   {
     _output = "Running...";
     try
     {
-    var options = (!string.IsNullOrWhiteSpace(_assemblies) && !string.IsNullOrWhiteSpace(_namespaces)) ? ScriptOptions.Default.AddReferences(_assemblies).WithImports(_namespaces) : null;
-    var script = CSharpScript.Create<string>(_code, options, globalsType: typeof(ScriptHost));
-    var host = new ScriptHost() {Args = _input};
-    var result = await script.RunAsync(host);
-    _output = result.ReturnValue;
+      var options = (!string.IsNullOrWhiteSpace(_assemblies) && !string.IsNullOrWhiteSpace(_namespaces)) ? ScriptOptions.Default.AddReferences(_assemblies).WithImports(_namespaces) : null;
+      var src = await _code.GetValue();
+      var script = CSharpScript.Create<string>(src, options, globalsType: typeof(ScriptHost));
+      var host = new ScriptHost() {Args = _input};
+      var result = await script.RunAsync(host);
+      _output = result.ReturnValue;
     }
     catch (Exception ex)
     {
