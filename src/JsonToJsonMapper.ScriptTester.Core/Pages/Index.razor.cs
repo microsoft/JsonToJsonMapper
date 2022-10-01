4f947e982391385cebf6ee93161b1c1c1f9836b8
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-10-01 12:29:57 +0100

    - run script

--- src/JsonToJsonMapper.ScriptTester.Core/Pages/Index.razor.cs
+++ src/JsonToJsonMapper.ScriptTester.Core/Pages/Index.razor.cs
@@ -1,4 +1,7 @@
-﻿namespace JsonToJsonMapper.ScriptTester.Core.Pages;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace JsonToJsonMapper.ScriptTester.Core.Pages;
 
 public sealed partial class Index
 {
@@ -9,9 +12,14 @@ public sealed partial class Index
   private string _output { get; set; }
   private bool _canRun => !string.IsNullOrWhiteSpace(_code) && !string.IsNullOrWhiteSpace(_input);
 
-  private Task OnRun()
+  private async Task OnRun()
   {
-    _output = $"Run:  {_code}{Environment.NewLine}+{Environment.NewLine}{_input}";
-    return Task.CompletedTask;
+    _output = "Running...";
+
+    var options = (!string.IsNullOrWhiteSpace(_assemblies) && !string.IsNullOrWhiteSpace(_namespaces)) ? ScriptOptions.Default.AddReferences(_assemblies).WithImports(_namespaces) : null;
+    var script = CSharpScript.Create<string>(_code, options, globalsType: typeof(ScriptHost));
+    var host = new ScriptHost() {Args = _input};
+    var result = await script.RunAsync(host);
+    _output = result.ReturnValue;
   }
 }
